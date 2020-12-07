da43d81152853bf51a417d7785bf5540d38e8829
Peter Hu <peterbohai@gmail.com>
2020-12-06 17:35:01 -0800

    Complete Part 4 Exercises

--- rango/views.py
+++ rango/views.py
@@ -10,4 +10,7 @@ def index(request):
 
 
 def about(request):
-    return HttpResponse('<a href="/rango/">Index</a><br/>Rango says here is the about page.')
+    context_dict = {
+        'boldmessage': 'This tutorial has been put together by Peter Hu'
+    }
+    return render(request, 'rango/about.html', context=context_dict)
